6e8ff198d9c4c9607b961aa9d3ba97b64bc42326
khalid fifel <fifle.khalid99@gmail.com>
2023-02-20 17:31:00 +0100

    add the model

---
+++ app/Models/Comment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'content',
+    ];
+
+
+    public function article(){
+        return $this->belongsTo(Article::class);
+    }
+}

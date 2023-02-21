663c41ed0a1e8556cf94ad50f110889a41b10b48
hamza harrass <hamzaharrass05@gmail.com>
2023-02-21 17:19:13 +0100

    function create category is added

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,3 +34,13 @@ Route::group([
     Route::post('me', [AuthController::class, 'me']);
 
 });
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+   Route::post('create',[CategoryController::class , 'create']);
+});
+

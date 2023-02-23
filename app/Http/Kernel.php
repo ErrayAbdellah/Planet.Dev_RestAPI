f36e2ba8d4c903a48c50f5086c666c3411d7fc6c
khalid fifel <fifle.khalid99@gmail.com>
2023-02-23 17:01:41 +0100

    add the isUserMiddleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'isAdmin' => \App\Http\Middleware\IsAdminMiddleware::class,
+        'isUser' => \App\Http\Middleware\IsUserMiddleware::class,
 
     ];
 }
